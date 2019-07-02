d8591d4d2bf27d0baae05f701ee40835a97ae839
Henrik Helenius <henrik@codeberry.fi>
2019-07-02 17:33:38 +0300

    Changed to classes, everything has forms

--- src/housing-benefits/housing-benefits.js
+++ src/housing-benefits/housing-benefits.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import './housing-benefits.scss';
 
-function HousingBenefits() {
-	return (
-		<section className="housing-benefits">
-			Kapser
-		</section>
-	);
+class HousingBenefits extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			children: 0,
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	//// Own business logic
+
+	// Change name of this
+	calculateShit() {
+		// this state is a string, remember that
+		return +this.state.children + 1;
+	}
+
+	//// Events
+
+	handleChange(event) {
+		this.setState({value: event.target.value});
+	}
+
+	handleSubmit(event) {
+		const result = this.calculateShit();
+		alert('Result is: ' + result); // TODO: set to state or something
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<section className="housing-benefits">
+
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="number"
+						min="0"
+						max="10"
+						placeholder="Children"
+						onChange={e => this.setState({children: e.target.value})}
+						value={this.state.children}
+					/>
+					<input type="submit" value="Submit" />
+				</form>
+
+			</section>
+		);
+	}
 }
+
+/* Handling inputs
+1. add to this.state a property x
+2. copy input, change type and other needed attributes
+3. change value={this.state.x}
+4. change onChange={e => this.setState({x: e.target.value})}
+ */
+
+/*
+barn
+vuxna
+sammanlagda inkomster
+kommun
+vuokra
+vattenavgift
+uppvärmningskostnader
+*/
 
 export default HousingBenefits;
--- src/student-benefits/student-benefits.js
+++ src/student-benefits/student-benefits.js
@@ -1,12 +1,73 @@
 import React from 'react';
 import './student-benefits.scss';
 
-function StudentBenefits() {
-	return (
-		<section className="student-benefits">
-			Basti
-		</section>
-	);
+class StudentBenefits extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			children: 0,
+		};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	//// Own business logic
+
+	// Change name of this
+	calculateShit() {
+		// this state is a string, remember that
+		return +this.state.children + 1;
+	}
+
+	//// Events
+
+	handleChange(event) {
+		this.setState({children: event.target.value});
+	}
+
+	handleSubmit(event) {
+		const result = this.calculateShit();
+		alert('Result is: ' + result); // TODO: set to state or something
+		event.preventDefault();
+	}
+
+	render() {
+		return (
+			<section className="student-benefits">
+
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="number"
+						min="0"
+						max="10"
+						placeholder="Children"
+						onChange={e => this.setState({children: e.target.value})}
+						value={this.state.children}
+					/>
+					<input type="submit" value="Submit" />
+				</form>
+
+			</section>
+		);
+	}
 }
+
+/* Handling inputs
+1. add to this.state a property x
+2. copy input, change type and other needed attributes
+3. change value={this.state.x}
+4. change onChange={e => this.setState({x: e.target.value})}
+ */
+
+/*
+ålder
+checkbox oppilaitos 2. / korekakoulu
+checkbox bor ensam / föräldrar
+föräldrarnas inkomst
+gift
+barn
+när skolan börjar
+*/
 
 export default StudentBenefits;
