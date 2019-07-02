a8589b9577b05bcaf9e46982313526c6a93a91c7
Sebastian Berner <sebastian.berner@aalto.fi>
2019-07-02 19:21:31 +0300

    2 timmar kvaaaaaaar

--- src/student-benefits/student-benefits.js
+++ src/student-benefits/student-benefits.js
@@ -1,59 +1,98 @@
 import React from 'react';
 import './student-benefits.scss';
 
-import saveStateToStorage from "../localStorage.helper";
+const Checkbox = (props) => {
+	return(
+	<div>
+	<p> Independent?</p>
+	<input type="checkbox" name={props.name} />
+	</div>
+	)
+}
 
 class StudentBenefits extends React.Component {
-	// For local storage
-	componentName = 'StudentBenefits';
+    constructor(props) {
+        super(props);
+        this.state = {
+            children: false,
+            isUniversity: false,
+            age: 0,
+            parentIncome: 0,
+            ownIncome: 0,
+            isIndependent: false,
+            isMarried: false,
+            studyBegin: "0.0.0",
+            credits: 0,
+        };
 
-	constructor(props) {
-		super(props);
-		this.state = {
-			children: 0,
-		};
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-		this.handleSubmit = this.handleSubmit.bind(this);
+    //// Own business logic
+
+    //Only for 300 Yleinen tutkimus (add support for others later)
+    //Alempi, Ylempi, Total.
+    supportMonths = [30, 21, 48];
+
+	//1.8.2019 alkaen
+	parentIncomeInfluence() {
+		const { parentIncome } = this.state;
+		if(parentIncome < 41400) {
+			return 100; //not real value
+		}
+		else {
+			return 0
+		}
 	}
 
-	//// Own business logic
+    opintoTuki = () => {
+        const { children, age, isMarried, isIndependent } = this.state;
+		if(children) {
+			return +325.28;
+		}
+		else if(isMarried) {
+			return +250.28;
+		}
+		else if( age >= 17 && age < 20 && !isIndependent) {
+			return +38.66 + this.parentIncomeInfluence();
+		}
+		else if(age >= 20 && !isIndependent) {
+			return +81.39 + this.parentIncomeInfluence();
+		}
+		else if(age >= 18 && isIndependent) {
+			return +250.28;
+		}
+		else if(age === 17 && isIndependent) {
+			return +101.74 + this.parentIncomeInfluence();
+		}
+		else if(age < 17) {
+			return this.parentIncomeInfluence();
+		}
+		else {
+			return 0;
+		}
+    }
 
-	// Change name of this
-	calculateShit() {
-		// this state is a string, remember that
-		return +this.state.children + 1;
-	}
+    //// Events
 
-	//// Events
+    handleChange(event) {
+        this.setState({children: event.target.value});
+    }
 
-	handleSubmit(event) {
-		const result = this.calculateShit();
-		alert('Result is: ' + result); // TODO: set to state or something
-		event.preventDefault();
+    handleSubmit(event) {
+        const result = this.opintoTuki();
+        alert('Result is: ' + result); // TODO: set to state or something
+        event.preventDefault();
+    }
 
-		// Save to local storage when something has been changed
-		saveStateToStorage(this.state, this.componentName)
-	}
-
-	render() {
-		return (
-			<section className="student-benefits">
-
-				<form onSubmit={this.handleSubmit}>
-					<input
-						type="number"
-						min="0"
-						max="10"
-						placeholder="Children"
-						onChange={e => this.setState({children: e.target.value})}
-						value={this.state.children}
-					/>
-					<input type="submit" value="Submit" />
-				</form>
-
-			</section>
-		);
-	}
+    render() {
+        return (
+	 <section className="student-benefits">
+		<Checkbox name="independent" />
+     </section>
+        );
+    }
 }
 
 /* Handling inputs
@@ -61,7 +100,20 @@ class StudentBenefits extends React.Component {
 2. copy input, change type and other needed attributes
 3. change value={this.state.x}
 4. change onChange={e => this.setState({x: e.target.value})}
- */
+*/
+
+/*           <form onSubmit={this.handleSubmit}>
+                    <input
+                        type="number"
+                        min="0"
+                        max="10"
+                        placeholder="Children"
+                        onChange={e => this.setState({children: e.target.value})}
+                        value={this.state.children}
+                    />
+                    <input type="submit" value="Submit" />
+                </form>
+*/
 
 /*
 ålder
